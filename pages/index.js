903679bce4a4f31172f64b62b2cb4aaab76f5144
viniciusalmeidadev <viniciusalmeidamail@gmail.com>
2021-03-02 14:51:04 -0300

    'menu'

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,36 @@
+import styles from './styles.module.css';
+
+
+
+
 function Home(){
-    return <p>Bem vindo!</p>
+    return(
+        <div>
+        <Menu />
+        <p>OLa</p>
+        </div>
+    )
+}
+
+function Menu(){
+
+    return(
+        <div className={styles.menu}>
+            <div className={styles.logo}>
+                <p>Site LOGO</p>
+            </div>
+            <div className={styles.menuItems}>
+                <a href="#">Home</a>
+                <a href="#">Produtos</a>
+                <a href="#">Contato</a>
+                <a href="#">Marcas</a>
+
+            </div>
+            <a href="#" className={styles.dnone}>apareci</a>
+
+        </div>
+    )
+        
 }
 
 export default Home;
